38b73364ec7f24a3936738a65946daeffb090473
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-12-28 23:04:27 +0530

    user inputs

--- App.js
+++ App.js
@@ -1,16 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
 
   const [name, setName] = useState('Lakshit');
+  const [age, setAge] = useState(21);
+  const [sport, setSport] = useState('');
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hi I am {name} and I am 21</Text>
+      <Text style={styles.text}>Hi I am {name} and I am {age}</Text>
+      <Text style={styles.text}>I love playing : {sport}</Text>
       <View style={{ marginTop: 20 }}>
         <Button title="Update Name" onPress={() => setName('Lakshit Chiranjiv')} />
+      </View>
+      <View style={{ marginTop: 20 }}>
+        <Text>Enter sport</Text>
+        <TextInput style={styles.input} placeholder='e.g. Cricket' onChangeText={(val) => setSport(val)}/>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -28,5 +35,13 @@ const styles = StyleSheet.create({
   text: {
     color: 'purple',
     fontSize: 16,
+  },
+
+  input: {
+    borderWidth: 1,
+    borderColor: '#777',
+    padding: 8,
+    margin: 10,
+    width: 200,
   }
 });
