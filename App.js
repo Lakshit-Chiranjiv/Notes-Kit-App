4e56534d19016b56b7415d23c4ebbb0e9c826cb6
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-12-28 23:19:18 +0530

    rendering lists

--- App.js
+++ App.js
@@ -1,25 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ScrollView,
+} from "react-native";
 
 export default function App() {
-
-  const [name, setName] = useState('Lakshit');
+  const [name, setName] = useState("Lakshit");
   const [age, setAge] = useState(21);
-  const [sport, setSport] = useState('');
+  const [sport, setSport] = useState("");
+  const [people, setPeople] = useState([
+    "Lakshit",
+    "Chiranjiv",
+    "Lakshit Chiranjiv",
+  ]);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hi I am {name} and I am {age}</Text>
-      <Text style={styles.text}>I love playing : {sport}</Text>
-      <View style={{ marginTop: 20 }}>
-        <Button title="Update Name" onPress={() => setName('Lakshit Chiranjiv')} />
-      </View>
-      <View style={{ marginTop: 20 }}>
-        <Text>Enter sport</Text>
-        <TextInput style={styles.input} placeholder='e.g. Cricket' onChangeText={(val) => setSport(val)}/>
-      </View>
-      <StatusBar style="auto" />
+      <ScrollView>
+        <Text style={styles.text}>
+          Hi I am {name} and I am {age}
+        </Text>
+        <Text style={styles.text}>I love playing : {sport}</Text>
+        <View style={{ marginTop: 20 }}>
+          <Button
+            title="Update Name"
+            onPress={() => setName("Lakshit Chiranjiv")}
+          />
+        </View>
+        <View style={{ marginTop: 20 }}>
+          <Text>Enter sport</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="e.g. Cricket"
+            onChangeText={(val) => setSport(val)}
+          />
+        </View>
+        <View style={{ marginTop: 20 }}>
+          {people.map((person, index) => {
+            return (
+              <View key={index}>
+                <Text style={styles.listText}>{person}</Text>
+              </View>
+            );
+          })}
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -27,21 +56,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 
   text: {
-    color: 'purple',
+    color: "purple",
     fontSize: 16,
   },
 
   input: {
     borderWidth: 1,
-    borderColor: '#777',
+    borderColor: "#777",
     padding: 8,
     margin: 10,
     width: 200,
-  }
+  },
+
+  listText: {
+    color: "white",
+    backgroundColor: "purple",
+    padding: 10,
+    margin: 10,
+    borderRadius: 10,
+  },
 });
