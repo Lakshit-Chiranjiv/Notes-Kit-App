ebc66919dad682c3c6d4bc39d72b513985b72cf2
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-01-01 16:55:46 +0530

    header and title styles

--- components/Header.js
+++ components/Header.js
@@ -12,4 +12,20 @@ const Header = () => {
   )
 }
 
+const styles = StyleSheet.create({
+    header: {
+        height: 80,
+        paddingTop: 38,
+        paddingHorizontal: 10,
+        backgroundColor: '#40c2ac',
+    },
+
+    title: {
+        textAlign: 'center',
+        color: '#fff',
+        fontSize: 20,
+        fontWeight: 'bold',
+    }
+})
+
 export default Header
