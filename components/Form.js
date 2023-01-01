6d0efdbf9434dc20fbf0c6bb2c61624e4d38a5b7
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-01-01 19:31:46 +0530

    Form styles

--- components/Form.js
+++ components/Form.js
@@ -10,12 +10,32 @@ const Form = () => {
   return (
     <View style={styles.form}>
         <Text style={styles.title}>Enter note</Text>
-        <TextInput style={styles.inputTitle} placeholder="Title"/>
-        <TextInput style={styles.inputContent} placeholder="Content"/>
+        <TextInput style={styles.input} placeholder="Title"/>
+        <TextInput style={styles.input} placeholder="Content"/>
         <Button title="Add" color="#40c2ac"/>
     </View>
   )
 }
 
+const styles = StyleSheet.create({
+    form: {
+        padding: 40,
+        marginTop: 20,
+    },
+
+    title: {
+        fontSize: 16,
+    },
+
+    input: {
+        marginTop: 10,
+        marginBottom: 10,
+        padding: 8,
+        borderWidth: 1,
+        borderColor: '#ddd',
+        borderRadius: 6,
+    }
+
+})
 
 export default Form
