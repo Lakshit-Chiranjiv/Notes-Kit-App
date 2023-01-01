5d1f2669e5c97caf60389d28f4fa791229e5152c
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-01-01 16:47:29 +0530

    Header structure

---
+++ components/Header.js
@@ -0,0 +1,15 @@
+import {
+    StyleSheet,
+    Text,
+    View,
+  } from "react-native";
+
+const Header = () => {
+  return (
+    <View style={styles.header}>
+        <Text style={styles.title}>Notes Kit</Text>
+    </View>
+  )
+}
+
+export default Header
