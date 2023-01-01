dddecc1eb7e2a6f0b6bd6e5f00514a10ffe46e2e
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-01-01 16:39:01 +0530

    dummy notes and notes state

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   StyleSheet,
   Text,
@@ -6,9 +7,16 @@ import {
 
 export default function App() {
 
+  const [notes, setNotes] = useState([
+    { title: "Note 1", content: "Content 1", key: "1" },
+    { title: "Note 2", content: "Content 2", key: "2" },
+    { title: "Note 3", content: "Content 3", key: "3" },
+    { title: "Note 4", content: "Content 4", key: "4" },
+  ])
+
   return (
     <View style={styles.container}>
-      
+
     </View>
   );
 }
